9825efd738cd30915884da4d36a10f7fad1161e5
Patrick <Pat@lardner.eu>
2022-01-16 01:15:54 +0000

    Solver for Wordle
    Takes wrong letters, wrong position letters, and correct position letters to return possibilities.
    
    More information returns fewer possibilities

---
+++ Solver/main.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+let words = fs.readFileSync('./five-letter-words.txt', 'utf8');
+console.log(`Total word count: ${words.split('\n').length}`)
+
+//Enter letters which do not appear anywhere in the word
+wrongletters = '';
+
+//Enter letters which are correct, but do not appear in this position
+wrongpos1 = '';
+wrongpos2 = '';
+wrongpos3 = '';
+wrongpos4 = '';
+wrongpos5 = '';
+
+//Enter letters which are correct, and appear in this position
+correctpos1 = '';
+correctpos2 = '';
+correctpos3 = '';
+correctpos4 = '';
+correctpos5 = '';
+
+let regex = new RegExp(`${correctpos1.length === 1 ? correctpos1 : '[^' + wrongletters + wrongpos1 + ']'}${correctpos2.length === 1 ? correctpos2 : '[^' + wrongletters + wrongpos2 + ']'}${correctpos3.length === 1 ? correctpos3 : '[^' + wrongletters + wrongpos3 + ']'}${correctpos4.length === 1 ? correctpos4 : '[^' + wrongletters + wrongpos4 + ']'}${correctpos5.length === 1 ? correctpos5 : '[^' + wrongletters + wrongpos5 + ']'}\n`, 'g')
+
+let matches = words.match(regex);
+console.log(matches.length, matches);
