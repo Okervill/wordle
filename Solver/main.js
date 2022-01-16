3626fc06f96cbd57b1fcc056fe6195d55dd9bf9e
Patrick <Pat@lardner.eu>
2022-01-16 01:26:43 +0000

    moved the matches length to the end

--- Solver/main.js
+++ Solver/main.js
@@ -25,4 +25,4 @@ correctpos5 = '';
 let regex = new RegExp(`${correctpos1.length === 1 ? correctpos1 : '[^' + wrongletters + wrongpos1 + ']'}${correctpos2.length === 1 ? correctpos2 : '[^' + wrongletters + wrongpos2 + ']'}${correctpos3.length === 1 ? correctpos3 : '[^' + wrongletters + wrongpos3 + ']'}${correctpos4.length === 1 ? correctpos4 : '[^' + wrongletters + wrongpos4 + ']'}${correctpos5.length === 1 ? correctpos5 : '[^' + wrongletters + wrongpos5 + ']'}\n`, 'g')
 
 let matches = words.match(regex);
-console.log(matches.length, matches);
+console.log(matches, matches.length);
