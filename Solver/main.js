bc9700bb600587f0440f36dfd8b5ae54ef38e69d
Patrick <16062927+Okervill@users.noreply.github.com>
2022-01-20 05:31:49 +0000

    Update main.js

--- Solver/main.js
+++ Solver/main.js
@@ -6,21 +6,21 @@ let words = fs.readFileSync('./five-letter-words.txt', 'utf8');
 console.log(`Total word count: ${words.split('\n').length}`)
 
 //Enter letters which do not appear anywhere in the word    
-wrongletters = 'lgtmuthnease\r\n';
+wrongletters = '\n';
 
 //Enter letters which are correct, but do not appear in this position
-wrongpos1 = 'o';
-wrongpos2 = 'io';
-wrongpos3 = 'ir';
-wrongpos4 = 'ov';
-wrongpos5 = 'r';
+wrongpos1 = '';
+wrongpos2 = '';
+wrongpos3 = '';
+wrongpos4 = '';
+wrongpos5 = '';
 
 //Enter letters which are correct, and appear in this position
 correctpos1 = '';
 correctpos2 = '';
-correctpos3 = 'o';
+correctpos3 = '';
 correctpos4 = '';
-correctpos5 = 'y';
+correctpos5 = '';
  
 let regex = new RegExp(`${correctpos1.length === 1 ? correctpos1 : '[^' + wrongletters + wrongpos1 + ']'}${correctpos2.length === 1 ? correctpos2 : '[^' + wrongletters + wrongpos2 + ']'}${correctpos3.length === 1 ? correctpos3 : '[^' + wrongletters + wrongpos3 + ']'}${correctpos4.length === 1 ? correctpos4 : '[^' + wrongletters + wrongpos4 + ']'}${correctpos5.length === 1 ? correctpos5 : '[^' + wrongletters + wrongpos5 + ']'}`, 'g')
 
